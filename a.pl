650a157046dc73bfa1c6bc9e8a55654afb4f34f0
mboschini <matteo.boschini@gmail.com>
2018-11-20 11:13:03 +0100

    bug fix for DOS formatting

--- a.pl
+++ a.pl
@@ -8,16 +8,17 @@ if($ARGV[0] !~ /www\.cert-pa\.it/) {
 }
 
 $urlo=$ARGV[0];
-print $urlo;
 $u1 = URI->new($urlo);
 $ioc=lc(basename($u1->path()));
 system("wget -q -O $ioc $urlo");
-print "HASR IoC $ioc\n";
+system("dos2unix $ioc");
+##print "HASR IoC $ioc\n";
 open(TEMP,">temp");
 open(IOC,"<$ioc");
 while(<IOC>) {
  chomp;
  print TEMP "$_;",$ioc," CERT-PA\n";
 }
+$new_ioc='cpa-'.$ioc;
 system("mv temp $ioc");
-system("mv $ioc iocs/$ioc");
+system("mv $ioc iocs/$new_ioc");
---
+++ tutti.sh
@@ -0,0 +1,9 @@
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/10/IoC_2695_20181023_HASHr_MD5.txt
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/10/IoC_2709_globale_HASHr.txt
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/10/IoC_Campagna_sentenze_legali_25-10-2018_HASHr_SHA256.txt
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/10/IoC_HASHr_AzoRult_15_23-10-2018.txt
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/11/IoC_Hashr_TT2737_SHA256.txt
+perl a.pl  https://www.cert-pa.it/wp-content/uploads/2018/11/IoC_HASHr_TT2743.txt
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/11/IoC_HASHr_TT2743.txt
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/11/IoC_HASHr_TT2748.txt
+perl a.pl https://www.cert-pa.it/wp-content/uploads/2018/11/IoC_Hashr_TT2756_20181119_SHA256.txt
